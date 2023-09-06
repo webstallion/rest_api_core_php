bb75a2c0b8cfb94a756e91f8a49797a13a74e250
jangal mangal <janagal@mangal>
2023-09-06 17:13:24 +0530

    new code

--- api_search.php
+++ api_search.php
@@ -1,13 +1,14 @@
 <?php
 header('Content-Type: application/json');
 header('Acess-Control-Allow-Origin: *');
-$data=json_decode(file_get_contents("php://input"), true); 
+header('Acess-Control-Allow-Methods: GET');
+// $data=json_decode(file_get_contents("php://input"), true); 
 include "config.php";
-$user_id=$data['id'];
-$sql="Select * from users where id = '$user_id'";
+
+$search_value=$_GET['search'];//$data['search'];
+$sql="Select * from users where firstname like '%{$search_value}%'";
 $result=mysqli_query($con,$sql) or die("Sql query failed");
 if(mysqli_num_rows($result)>0){
-	//$output=mysqli_fetch_all($result, );
 	$output=mysqli_fetch_all($result, MYSQLI_ASSOC);
 	echo json_encode($output);
 }else{
